cbf290d85e4774089a1fb60c75d85875d719f5d2
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-27 00:03:31 +0900

    scaffold でuserモデル・コントローラーの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+     else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+
+
+
+  private
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password_digest)
+    end
+end
